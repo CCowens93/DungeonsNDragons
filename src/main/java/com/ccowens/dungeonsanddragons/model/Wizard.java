70061a432d580d1e6c5a21646cc2787d6a53615c
Casey Cowens <cassandracowens@Cassandras-MacBook-Pro.local>
2020-08-27 11:54:42 -0400

    created Wizard Group, User, and Event models

---
+++ src/main/java/com/ccowens/dungeonsanddragons/model/Wizard.java
@@ -0,0 +1,35 @@
+package com.ccowens.dungeonsanddragons.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsContructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name="user_group")
+
+public class Wizard {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String name;
+    private String classAndLevel;
+    private String race;
+    private String background;
+    private String alignment;
+    private String playerName;
+    private int experiencePoints;
+    @ManyToOne(cascade=CascadeType.PERSIST)
+    private User user;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    private Set<Event> events;
+}
