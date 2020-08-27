e6399f3fea1ea04b5afcef1986b6e8b0d608904f
Casey Cowens <cassandracowens@Cassandras-MacBook-Pro.local>
2020-08-27 11:10:03 -0400

    inital commit

---
+++ src/main/java/com/ccowens/dungeonsanddragons/DungeonsanddragonsApplication.java
@@ -0,0 +1,13 @@
+package com.ccowens.dungeonsanddragons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsanddragonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonsanddragonsApplication.class, args);
+	}
+
+}
