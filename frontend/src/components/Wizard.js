b64e7c5fe64e820422d4cc24841d03dc906aa7a9
Casey Cowens <cassandracowens@Cassandras-MacBook-Pro.local>
2020-08-30 01:03:41 -0400

    css added to wizard character page

--- frontend/src/components/Wizard.js
+++ frontend/src/components/Wizard.js
@@ -44,78 +44,120 @@ class Wizard extends Component{
                 const alignment = `${wizard.alignment}`;
                 const playerName = `${wizard.playerName}`;
                 const experiencePoints =`${wizard.experiencePoints}`;
+                const age = `${wizard.age}`;
+                const eyes = `${wizard.eyes}`;
+                const height = `${wizard.height}`;
+                const skin = `${wizard.skin}`;
+                const weight = `${wizard.weight}`;
+                const hair = `${wizard.hair}`;
+                const personalityTraits = `${wizard.personalityTraits}`;
+                const ideals = `${wizard.ideals}`;
+                const bonds = `${wizard.bonds}`;
+                const flaws = `${wizard.flaws}`;
+                const featuresAndTraits = `${wizard.featuresAndTraits}`;
 
-
-            return <tr key={wizard.id}>
-
-                  <td style={{whiteSpace: 'nowrap'}}>{wizard.name}</td>
-                  <td>{classAndLevel}</td>
-                  <td>{race}</td>
-                  <td>{background}</td>
-                  <td>{alignment}</td>
-                  <td>{playerName}</td>
-                  <td>{experiencePoints}</td>
+            return <tr className="wiz" key={wizard.id}>
+            <br></br>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Name</th>
+                  <td width="150px" height="100px" style={{whiteSpace: 'wrap'}}>{wizard.name}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Class and Level</th>
+                  <td width="150px" height="100px">{classAndLevel}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Race</th>
+                  <td width="150px" height="100px">{race}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Background</th>
+                  <td width="150px" height="100px">{background}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Alignment</th>
+                  <td width="150px" height="100px">{alignment}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Player Name</th>
+                  <td width="150px" height="100px">{playerName}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Experience Points</th>
+                  <td width="150px" height="100px">{experiencePoints}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Age</th>
+                  <td width="150px" height="100px">{age}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Eyes</th>
+                  <td width="150px" height="100px">{eyes}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Height</th>
+                  <td width="150px" height="100px">{height}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Skin</th>
+                  <td width="150px" height="100px">{skin}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Weight</th>
+                  <td width="150px" height="100px">{weight}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="150px" height="100px">Hair</th>
+                  <td width="150px" height="100px">{hair}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="400px" height="100px">Personality Traits</th>
+                  <td width="400px" height="100px">{personalityTraits}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="400px" height="100px">Ideals</th>
+                  <td width="400px" height="100px">{ideals}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="400px" height="100px">Bonds</th>
+                  <td width="400px" height="100px">{bonds}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="400px" height="100px">Flaws</th>
+                  <td width="400px" height="100px">{flaws}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="400px" height="100px">Features & Traits</th>
+                  <td width="400px" height="200px">{featuresAndTraits}</td>
+            </div>
+            <div className="wizzz">
+                  <th width="100px" height="100px">Action</th>
+                  <td width="100px" height="100px">
+                     <ButtonGroup>
+                         <Button size="sm" color="primary" tag={Link} to={"/wizard/" + wizard.id}>Edit</Button>
+                         <Button size="sm" color="danger" onClick={() => this.remove(wizard.id)}>Delete</Button>
+                     </ButtonGroup>
+                  </td>
+                  <br></br>
+            </div>
                 </tr>
+
               });
 
-              const wizardList2 = wizard.map(wizard => {
-                    const age = `${wizard.age}`;
-                    const eyes = `${wizard.eyes}`;
-                    const height = `${wizard.height}`;
-                    const skin = `${wizard.skin}`;
-                    const weight = `${wizard.weight}`;
-                    const hair = `${wizard.hair}`;
 
-              return <tr key={wizard.id}>
-                        <td>{age}</td>
-                        <td>{eyes}</td>
-                        <td>{height}</td>
-                        <td>{skin}</td>
-                        <td>{weight}</td>
-                        <td>{hair}</td>
-                     </tr>
-              })
-
-              const wizardList3 = wizard.map(wizard => {
-                    const personalityTraits = `${wizard.personalityTraits}`;
-                    const ideals = `${wizard.ideals}`;
-                    const bonds = `${wizard.bonds}`;
-                    const flaws = `${wizard.flaws}`;
-                    const featuresAndTraits = `${wizard.featuresAndTraits}`;
-
-              return <tr key={wizard.id}>
-                    <td>{personalityTraits}</td>
-                    <td>{ideals}</td>
-                    <td>{bonds}</td>
-                    <td>{flaws}</td>
-                    <td>{featuresAndTraits}</td>
-                    <td>
-                    <ButtonGroup>
-                      <Button size="sm" color="primary" tag={Link} to={"/wizard/" + wizard.id}>Edit</Button>
-                      <Button size="sm" color="danger" onClick={() => this.remove(wizard.id)}>Delete</Button>
-                    </ButtonGroup>
-                  </td>
-                </tr>
-              })
 
               return (
                     <div>
                       <AppNavbar/>
                       <Container fluid>
-                        <div className="float-right">
+                        <div>
                           <Button color="success" tag={Link} to="/wizard/new">Add Wizard</Button>
                         </div>
                         <h3>Wizard Characters</h3>
-                        <Table className="mt-4">
+                        <div className="wiz">
+                        <Table className="wiz">
                           <thead>
-                          <tr>
-                            <th width="10%">Name</th>
-                            <th width="10%">Class and Level</th>
-                            <th width="10%">Race</th>
-                            <th width="10%">Background</th>
-                            <th width="10%">Alignment</th>
-                            <th width="10%">Player Name</th>
-                            <th width="10%">Experience Points</th>
+                          <tr className="wiz">
 
                           </tr>
 
@@ -125,37 +167,8 @@ class Wizard extends Component{
                           </tbody>
                         </Table>
 
-                        <Table className="mt-4">
-                            <thead>
-                                <tr>
-                                    <th width="10%">Age</th>
-                                    <th width="10%">Eyes</th>
-                                    <th width="10%">Height</th>
-                                    <th width="10%">Skin</th>
-                                    <th width="10%">Weight</th>
-                                    <th width="10%">Hair</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {wizardList2}
-                            </tbody>
-                        </Table>
 
-                        <Table className="mt-4">
-                            <thead>
-                                <tr>
-                                    <th width="10%">Personality Traits</th>
-                                    <th width="10%">Ideals</th>
-                                    <th width="10%">Bonds</th>
-                                    <th width="10%">Flaws</th>
-                                    <th width="10%">Features & Traits</th>
-                                    <th width="10%">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            {wizardList3}
-                            </tbody>
-                        </Table>
+                        </div>
 
                       </Container>
 
